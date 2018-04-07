434a9769ba4ba9c237d45394d3463c1a979b86b1
Marina Lunts <earine@macbook-air-maryna.local>
2018-04-07 23:32:51 +0300

    Login view

---
+++ Exportera/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exportera
+//
+//  Created by Marina Lunts on 4/7/18.
+//  Copyright © 2018 Marina Lunts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
